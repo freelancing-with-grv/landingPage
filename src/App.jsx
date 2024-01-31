32b3690af242c277136677b523176f01b79bf091
gourav mahobe <108221462+grv24@users.noreply.github.com>
2024-01-31 05:33:50 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Button from '@mui/material/Button';
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-center text-3xl font-bold underline">
+      Hello From tailwind
+    </h1>
+    <Button  variant="contained">Hello From mui</Button>
+    </>
+  )
+}
+
+export default App
